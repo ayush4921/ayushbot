38954b7885e9ec67982313e1628d081a6709777c
Ayush Garg <62711517+ayush4921@users.noreply.github.com>
2020-11-13 00:21:10 +0530

    ss

--- ayushbot/ayushbot.py
+++ ayushbot/ayushbot.py
@@ -11,6 +11,30 @@ import requests
 import chromedriver_autoinstaller
 
 
+import time
+import datetime as dt
+import json
+import os
+import requests
+import shutil
+import pickle
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from urllib.parse import urlencode
+try:
+    from bs4 import BeautifulSoup
+except ModuleNotFoundError:
+    print("Beautiful Soup Library is reqired to make this library work(For getting participants list for the specified group).\npip3 install beautifulsoup4")
+
 chromedriver_autoinstaller.install()
 
 def ayushbot(receiver):
