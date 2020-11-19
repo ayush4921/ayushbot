f12866bd158606ca8449c237ac2be5dfe8c41c45
Ayush Garg <62711517+ayush4921@users.noreply.github.com>
2020-11-19 13:18:43 +0530

    Made it fullproof

--- ayushbot/ayushbot.py
+++ ayushbot/ayushbot.py
@@ -29,6 +29,8 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+
+
 from urllib.parse import urlencode
 try:
     from bs4 import BeautifulSoup
@@ -38,95 +40,44 @@ except ModuleNotFoundError:
 chromedriver_autoinstaller.install()
 
 def ayushbot(receiver):
-    client_name = str(random.randint(1337, 9696913371337)).rjust(13, '0')
-    url = 'https://www.pandorabots.com/mitsuku/'
-    session = requests.Session()
-    session.headers.update({
-        'User-Agent': ' '.join(('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2)',
-                                'AppleWebKit/537.36 (KHTML, like Gecko)',
-                                'Chrome/72.0.3626.119 Safari/537.36')),
-        'Referer': url
-    })
-
-    main_page = session.get(url).text
-    main_soup = bs4.BeautifulSoup(main_page, 'lxml')
-    botkey = re.search(r'PB_BOTKEY: "(.*)"', main_page).groups()[0]
-
+    
     whatsapp = WhatsApp(100, session="mysession")
     whatsapp.send_message(receiver, "AyushBOT will serve you till Ayush comes online and answers your questions. As it is still very young AyushBOT can act very stupid sometimes. Please dont mind it. Start talking with AyushBOT below")
     newmessages=[]
     responses=[]
+    chrome_options = Options()
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.pandorabots.com/mitsuku/")
+    content = driver.find_element_by_xpath("//button[@class='pb-widget__launcher pb-widget__launcher__open']")
+    content.click()
     while 1==1:
         messages = whatsapp.get_last_message_for(receiver)
         newmessages.append(messages)
-        print(newmessages)
         if len(newmessages)>1:
             if len(newmessages)>10:
                 newmessages=newmessages[:-5]
 
             if newmessages[-1][-1] != newmessages[-2][-1]:
+                
+                query=str(messages[-1])
+                
+
+                content2 = driver.find_element_by_xpath("//input[@class='pb-widget__input__message__input']")
+                time.sleep(2)
+
+                content2.send_keys(query)
+                content2.submit()
+                time.sleep(5)
                 try:
-                    query=str(messages[-1])
-                    response_raw = session.post(
-                    f'https://miapi.pandorabots.com/talk?'
-                    f'botkey={botkey}&'
-                    f'input={query}&'
-                    f'client_name={client_name}&'
-                    f'sessionid=null&'
-                    f'channel=6').text
-                    try:
-                        response_json = json.loads(response_raw)
-                    except json.JSONDecodeError:
-                        # Sometimes Mitsuku sends stuff like pictures.
-                        response_json = {'responses': ["<i can't understand it, bro>"]}
-                    # If the query is too long, Mitsuku answers in several lines.  Here
-                    # I'll just put it into paragraphs.
-                    response = '\n\n'.join(response_json['responses'])
-                    print(response)
+                    content3 = driver.find_elements_by_xpath("//div[@class='pb-chat-bubble pb-chat-bubble__bot']")
+                    answer=content3[-1].text
 
                 except:
-                    client_name = str(random.randint(1337, 9696913371337)).rjust(13, '0')
+                    answer='Indeed'
+                response=answer
+                
 
-                    query=str(messages[-1])
-                    response_raw = session.post(
-                    f'https://miapi.pandorabots.com/talk?'
-                    f'botkey={botkey}&'
-                    f'input={query}&'
-                    f'client_name={client_name}&'
-                    f'sessionid=null&'
-                    f'channel=6').text
-                    try:
-                        response_json = json.loads(response_raw)
-                    except json.JSONDecodeError:
-                        # Sometimes Mitsuku sends stuff like pictures.
-                        response_json = {'responses': ["<i can't understand it, bro>"]}
-                    # If the query is too long, Mitsuku answers in several lines.  Here
-                    # I'll just put it into paragraphs.
-                    response = '\n\n'.join(response_json['responses'])
-                    print(response)
-                if '{"status": "error","message":"401 unauthorized"}' in response:
-                    response=response.replace('{"status": "error","message":"401 unauthorized"}','')
-                if 'Kuki' in response:
-                    response=response.replace('Kuki','AyushBot')
-                if bool(response)==False:
-                    client_name = str(random.randint(1337, 9696913371337)).rjust(13, '0')
-
-                    query=str(messages[-1])
-                    response_raw = session.post(
-                    f'https://miapi.pandorabots.com/talk?'
-                    f'botkey={botkey}&'
-                    f'input={query}&'
-                    f'client_name={client_name}&'
-                    f'sessionid=null&'
-                    f'channel=6').text
-                    try:
-                        response_json = json.loads(response_raw)
-                    except json.JSONDecodeError:
-                        # Sometimes Mitsuku sends stuff like pictures.
-                        response_json = {'responses': ["<i can't understand it, bro>"]}
-                    # If the query is too long, Mitsuku answers in several lines.  Here
-                    # I'll just put it into paragraphs.
-                    response = '\n\n'.join(response_json['responses'])
 
                 if '{"status": "error","message":"401 unauthorized"}' in response:
                     response=response.replace('{"status": "error","message":"401 unauthorized"}','')
@@ -141,8 +92,5 @@ def ayushbot(receiver):
                         whatsapp.send_message(receiver, f'{responses[-1]}')
                 elif len(responses)==1:
                     whatsapp.send_message(receiver, f'{responses[-1]}')
-                        
-
-                
 
 
--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ long_description = f.read()
 setup(
   name = 'ayushbot',         # How you named your package folder (MyLib)
   packages = ['ayushbot'],   # Chose the same as "name"
-  version = '0.0.2.2',      # Start with a small number and increase it with every change you make
+  version = '0.0.2.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Interfaces mitsuku or kuki chatbot with whatsapp',   # Give a short description about your library
   author = 'Ayush Garg',                   # Type in your name
   author_email = 'themoviechannel77@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ayush4921/ayushbot',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ayush4921/ayushbot/archive/0.0.2.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ayush4921/ayushbot/archive/0.0.2.3.tar.gz',    # I explain this later on
   keywords = ['whatsapp','mitsuku','kuki','chatbot'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'beautifulsoup4==4.6.3',
